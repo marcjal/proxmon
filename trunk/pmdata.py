18c117b8597777b929cca5c6fb8f23a7d7cbd2ff
jwilkins <jwilkins@c2b24d34-6e2b-0410-96f4-6d69d1846066>
2007-08-09 07:22:55 +0000

    improved hash and b64 enc/dec

--- trunk/pmdata.py
+++ trunk/pmdata.py
@@ -3,7 +3,7 @@
 """
 Datastore populated by proxy log parsers and consumed by modules
 """
-import md5, base64, sha
+import md5, base64, sha, re
 
 def md5sum(data):
 	m = md5.new(data)
@@ -13,20 +13,74 @@ def sha1sum(data):
 	s = sha.new(data)
 	return s.hexdigest()
 
+# A series of base64 decodes to handle all of the weird variants commonly seen
+# stock: ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/
+#        padding: =
+	# misc: no padding
+	# others: padding - instead of =
+def b64d(data):
+	return base64.standard_b64decode(data)
+
+def b64d_url(data):
+	return base64.urlsafe_b64decode(data)
+
+def b64d_regex(data):
+	return base64.b64decode(data, '!-')
+
+def b64d_xml1(data):
+	return base64.b64decode(data, '_-')
+
+def b64d_xml2(data):
+	return base64.b64decode(data, '._')
+
+def b64d_miscpad1(data):
+	if data[-2:] == '--':
+		return base64.b64decode(re.sub('-','=', data))
+
+def b64d_nopad(data):
+	if data[-1] != data[-2]:
+		return base64.b64decode(data+'==')
+
 class pmdata(object):
 	def add(self, key, data, dest):
-		if key in dest: dest[key].append(data)
+		# see if it's b64 encoded and handle
+		if key[-2:] == '--' or key[-2:] == '==':
+			for f in [b64d, b64d_url, b64d_regex, b64d_xml1, b64d_xml2, 
+					  b64d_miscpad1]:
+				try:
+					dec = f(key)
+					if not dec: continue
+					if dec in dest: dest[dec].append(data)
+					else: dest[dec] = [data]
+				except TypeError, e: 
+					if e.message == 'Incorrect padding': pass
+
+		# XXX handle b64d_nopad - will work on most strings, but result is
+		# generally wrong
+
+		# key already exists
+		if key in dest: 
+			if type(dest[key]) == list:
+				dest[key].append(data)
+			elif type(dest[key]) == str:
+				redir = dest[key]
+				print "dest[key] is a str (key %s), appending at redir %s" % (key, redir)
+				dest[redir].append(data)
+			else:
+				print '[x] unexpected error in pmdata.add()'
+				print '    type is: %s, value %s' % (type(dest[key]), dest[key])
+		# key doesn't exist
 		else: dest[key] = [data]
+
+		# try hashing the value and see if the hash matches anything we've seen
 		for f in [md5sum, sha1sum, base64.b64encode]:
-			if f(key) in dest:
-				dest[key].extend(dest[f(key)])
-				del dest[f(key)]
-		for f in [base64.b64decode]:
-			try:
-				if f(key) in dest:
-					dest[f(key)].extend(dest[key])
-					del dest[key]
-			except: pass
+			h = f(key)
+			if not h: continue
+			if h in dest and type(dest[h]) == list:
+				dest[key].extend(dest[h])
+				del dest[h]
+				dest[h] = key
+
 
 	def __init__(self):
 		self.Transactions = []
